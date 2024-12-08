3e2b18a55ce7eaf83d69d6f09ebd066d7428c276
Nirvika Adhikari <nirvikaadhikari99@gmail.com>
2024-12-08 22:43:03 +0545

    Create LogoutDialog.vue

---
+++ src/pages/LogoutDialog.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { useUserDetailsStore } from 'src/stores/userDetailsStore';
+import { useQuasar } from 'quasar';
+import { ref } from 'vue';
+import { useRouter } from 'vue-router';
+
+const store = useUserDetailsStore();
+const $q = useQuasar();
+const router = useRouter();
+
+const logout = () => {
+  $q.dialog({
+    title: 'Confirm Logout',
+    message: 'Are you sure you want to logout?',
+    cancel: true,
+    persistent: true,
+  }).onOk(() => {
+    store.logout();
+    router.push('/');
+  });
+};
+</script>
+
+<template>
+  <q-btn @click="logout" label="Logout" color="negative" />
+</template>
