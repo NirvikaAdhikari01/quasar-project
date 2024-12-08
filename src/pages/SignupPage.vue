0980249a48904583dbbd1469456419682eee402d
Nirvika Adhikari <nirvikaadhikari99@gmail.com>
2024-12-08 22:10:09 +0545

    Update SignupPage.vue

--- src/pages/SignupPage.vue
+++ src/pages/SignupPage.vue
@@ -1,32 +1,32 @@
 <script setup>
-import { userDetails } from 'src/stores/LoginRegistration';
+import { ref } from 'vue';
+import { useUserDetailsStore } from 'src/stores/userDetailsStore';
 import { useRouter } from 'vue-router';
-import { watch } from 'vue';
-const router=useRouter()
-const userdetails=userDetails()
-watch(()=>userdetails.name,(newVal,oldVal)=>{
-  console.log(`Name changed from ${oldVal} to ${newVal}`);
-})
-watch(()=>userdetails.isLoggedIn,(newVal,oldVal)=>{
-  console.log(`value changed from ${oldVal} to ${newVal}`)
-})
+
+const store = useUserDetailsStore();
+const router = useRouter();
+
+const name = ref('');
+const email = ref('');
+const password = ref('');
+const confirmPassword = ref('');
+
+const signup = () => {
+  // Pass values to the store's signup method
+  store.signup(router, name.value, email.value, password.value, confirmPassword.value);
+};
 </script>
+
 <template>
-<div class="flex flex-col justify-between items-center mt-[100px] space-y-10">
-  <div>
-    Registration Page
+  <div class="max-w-sm mx-auto p-6 border border-gray-300 rounded-md shadow-lg mt-10">
+    <h2 class="text-2xl font-bold mb-6 text-center">Sign Up</h2>
+    <input v-model="name" type="text" placeholder="Name" class="w-full p-3 mb-4 border border-gray-300 rounded-md" />
+    <input v-model="email" type="email" placeholder="Email" class="w-full p-3 mb-4 border border-gray-300 rounded-md" />
+    <input v-model="password" type="password" placeholder="Password" class="w-full p-3 mb-4 border border-gray-300 rounded-md" />
+    <input v-model="confirmPassword" type="password" placeholder="Confirm Password" class="w-full p-3 mb-4 border border-gray-300 rounded-md" />
+    <button @click="signup" :disabled="store.isSubmitting" class="w-full p-3 bg-blue-500 text-white rounded-md hover:bg-blue-600">
+      Sign Up
+    </button>
+    <p v-if="store.error" class="text-red-500 mt-4 text-center">{{ store.error }}</p>
   </div>
-  <div class="flex flex-col justify-between items-center">
-    <q-input placeholder="Enter your name" v-model="userdetails.name"/>
-    <q-input placeholder="Enter your email" v-model="userdetails.email"/>
-    <q-input placeholder="Enter your Password" v-model="userdetails.password" />
-    <q-input placeholder="Confirm your Password" v-model="userdetails.confirmPassword"/>
-  </div>
-  <div v-if="userdetails.error">
-   {{ userdetails.error }}
-  </div>
-  <div>
-    <q-btn label="Sign Up" @click="userdetails.signup(router)"/>
-  </div>
-</div>
 </template>
