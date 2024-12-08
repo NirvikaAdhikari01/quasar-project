c65d35c1b6ac626b11c6eedfc289572661a11dac
Nirvika Adhikari <nirvikaadhikari99@gmail.com>
2024-12-08 22:09:38 +0545

    Update LoginPage.vue

--- src/pages/LoginPage.vue
+++ src/pages/LoginPage.vue
@@ -1,27 +1,31 @@
 <script setup>
-import { userDetails } from 'src/stores/LoginRegistration';
-import { watch } from 'vue';
+import { ref } from 'vue';
+import { useUserDetailsStore } from 'src/stores/userDetailsStore';
 import { useRouter } from 'vue-router';
-const router=useRouter()
-const userdetails=userDetails()
-watch(()=>userdetails.isLoggedIn,(newVal,oldVal)=>{
-  console.log(`value changed from ${oldVal} to ${newVal}`)
-})
+
+const store = useUserDetailsStore();
+const router = useRouter();
+
+const name = ref('');
+const password = ref('');
+const errorMessage = ref('');
+
+const handleLogin = async () => {
+  const success = await store.login(name.value, password.value, router);
+  if (!success) {
+    errorMessage.value = store.error;
+  }
+};
 </script>
+
 <template>
-<div class="flex flex-col justify-between items-center mt-[100px]">
-  <div>
-    Login Page
+  <div class="max-w-sm mx-auto p-6 border border-gray-300 rounded-md shadow-lg mt-10">
+    <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
+    <div>
+      <input v-model="name" type="text" placeholder="Name" class="w-full p-3 mb-4 border border-gray-300 rounded-md" />
+      <input v-model="password" type="password" placeholder="Password" class="w-full p-3 mb-4 border border-gray-300 rounded-md" />
+      <button @click="handleLogin" class="w-full p-3 bg-green-500 text-white rounded-md hover:bg-green-600">Login</button>
+      <p v-if="errorMessage" class="text-red-500 mt-4 text-center">{{ errorMessage }}</p>
+    </div>
   </div>
-  <div class="flex flex-col justify-between items-center">
-    <q-input placeholder="Enter your name" v-model="userdetails.name"/>
-    <q-input placeholder="Enter your Password" v-model="userdetails.password" />
-  </div>
-  <div v-if="userdetails.error">
-   {{ userdetails.error }}
-  </div>
-  <div>
-    <q-btn label="Login" @click="userdetails.login(router)"/>
-  </div>
-</div>
 </template>
