cf6ad81157d8789a4fe6d5ed514be95a08d9696e
Nirvika Adhikari <nirvikaadhikari99@gmail.com>
2024-12-08 22:50:01 +0545

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,34 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/**/*.{html,js,ts,vue}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      animation: {
+        slideInLeft: 'slideInLeft 1s ease-out forwards',
+        slideInRight: 'slideInRight 1s ease-out forwards',
+        fadeInZigzag: 'fadeInZigzag 2s ease-out forwards',
+      },
+      keyframes: {
+        slideInLeft: {
+          '0%': { transform: 'translateX(-100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        slideInRight: {
+          '0%': { transform: 'translateX(100%)' },
+          '100%': { transform: 'translateX(0)' },
+        },
+        fadeInZigzag: {
+          '0%': { opacity: 0, transform: 'translateX(-50%)' },
+          '50%': { opacity: 1, transform: 'translateX(10%)' },
+          '100%': { opacity: 1, transform: 'translateX(0)' },
+        },
+      },
+      colors: {
+        gradientStart: '#003E5C',
+        gradientEnd: '#011926',
+      },
+    },
   },
   plugins: [],
-};
+}
